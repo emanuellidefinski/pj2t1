67fc56f37bb3f78e15b1d450800c8cbb3f620f8f
emanuellidefinski <emanuelli.definski@escola.pr.gov.br>
2025-06-18 12:04:24 -0300

    Add files via upload

---
+++ Emanuelli 2b/flashcard-aula_8/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'artista',
+    'Quem é billie eilish?',
+    'Billie Eilish é uma cantora e compositora estadunidense'
+)
+
+criaCartao(
+    'Genero musical',
+    'Qual é o genero musical que billie canta?',
+    'música pop'
+)
+
+criaCartao(
+    'Premios',
+    'quantos grammys a billie ganhou?',
+    'billie eilsih tem 9 grammys'
+)
+
+criaCartao(
+    'albuns',
+    'Quantos albuns musicais a billie tem?',
+    'Billie ja produziu juntamente com seu irmão 3 albuns de grande sucesso'
+)
